f6c097e1a0663aee096b8516ee30c1314ea780e6
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2020-11-25 13:29:27 +0100

    latest commit

---
+++ build/precache-manifest.95562960cd345bc0d9ec8daf2561b7d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2957ef4a16f391c7c09d893c26459eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9326c3ce2d36b6b7ca1c",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "087029f909143b5a686b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9326c3ce2d36b6b7ca1c",
+    "url": "/static/js/2.bd1c89c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd1c89c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087029f909143b5a686b",
+    "url": "/static/js/main.1af3b63b.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
